ffbc21be4d2b6684e76cf807cd2634dd5b10f707
hehe <hehe@gmail.com>
2025-03-19 11:03:25 +0100

    l'updateuu

--- LeTemps/Views/Profil/FriendRowView.swift
+++ LeTemps/Views/Profil/FriendRowView.swift
@@ -1,41 +1,18 @@
+//
+//  FriendRowView.swift
+//  LeTemps
+//
+//  Created by froehly jean-baptiste on 19/03/2025.
+//
+
 import SwiftUI
 
 struct FriendRowView: View {
-    var name: String
-    var icon: String
-    var screenTime: String
-
     var body: some View {
-        HStack {
-            Image(systemName: icon)
-                .resizable()
-                .frame(width: 40, height: 40)
-                .clipShape(Circle())
-
-            VStack(alignment: .leading) {
-                Text(name)
-                    .font(.headline)
-                Text(screenTime)
-                    .font(.subheadline)
-                    .foregroundColor(.gray)
-            }
-            .padding(.leading, 8)
-
-            Spacer()
-
-            HStack {
-                Image(systemName: "arrow.clockwise")
-                    .foregroundColor(.blue)
-                Image(systemName: "xmark")
-                    .foregroundColor(.red)
-            }
-        }
-        .padding()
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct FriendRowView_Previews: PreviewProvider {
-    static var previews: some View {
-        FriendRowView(name: "Mehdi", icon: "person", screenTime: "3h d'écran aujourd'hui")
-    }
+#Preview {
+    FriendRowView()
 }
--- LeTemps/Views/Profil/FriendsListView.swift
+++ LeTemps/Views/Profil/FriendsListView.swift
@@ -1,31 +1,18 @@
+//
+//  FriendsListView.swift
+//  LeTemps
+//
+//  Created by froehly jean-baptiste on 19/03/2025.
+//
+
 import SwiftUI
 
 struct FriendsListView: View {
     var body: some View {
-        List {
-            Section(header: Text("Trouver des amis")) {
-                TextField("Rechercher...", text: .constant(""))
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-            }
-
-            Section(header: Text("Vos Amis")) {
-                FriendRowView(name: "Mehdi", icon: "person", screenTime: "3h d'écran aujourd'hui")
-                FriendRowView(name: "Jamesse", icon: "person", screenTime: "7h d'écran aujourd'hui")
-
-                Section(header: Text("Favoris")) {
-                    FriendRowView(name: "Baptiste", icon: "person", screenTime: "")
-                    FriendRowView(name: "Max", icon: "person", screenTime: "")
-                    FriendRowView(name: "UG", icon: "person", screenTime: "")
-                    FriendRowView(name: "Piedslie", icon: "person", screenTime: "")
-                }
-            }
-        }
-        .listStyle(GroupedListStyle())
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct FriendsListView_Previews: PreviewProvider {
-    static var previews: some View {
-        FriendsListView()
-    }
+#Preview {
+    FriendsListView()
 }
