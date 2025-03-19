aabe24ec4fd07b1886dd6ed1fab2d09b2140cf68
hehe <hehe@gmail.com>
2025-03-19 11:18:13 +0100

    Accueil

---
+++ LeTemps/Views/Accueil/ProgressBarView.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressBarView.swift
+//  LeTemps
+//
+//  Created by froehly jean-baptiste on 19/03/2025.
+//
+
+import SwiftUI
+
+struct ProgressBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressBarView()
+}
---
+++ LeTemps/Views/Accueil/WeekDayView.swift
@@ -0,0 +1,18 @@
+//
+//  WeekDayView.swift
+//  LeTemps
+//
+//  Created by froehly jean-baptiste on 19/03/2025.
+//
+
+import SwiftUI
+
+struct WeekDayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeekDayView()
+}
