f187f77251226b2300a14a85bfac2cf46738ca36
sirthirrygolooo <sirthirrygoloooo@hehe.com>
2025-03-20 15:43:12 +0100

    hehe

---
+++ LeTemps/Views/Stats/PickupsView.swift
@@ -0,0 +1,18 @@
+//
+//  PickupsView.swift
+//  LeTemps
+//
+//  Created by froehly jean-baptiste on 20/03/2025.
+//
+
+import SwiftUI
+
+struct PickupsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PickupsView()
+}
---
+++ LeTemps/Views/Stats/ScreenTimeView.swift
@@ -0,0 +1,18 @@
+//
+//  ScreenTimeView.swift
+//  LeTemps
+//
+//  Created by froehly jean-baptiste on 20/03/2025.
+//
+
+import SwiftUI
+
+struct ScreenTimeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScreenTimeView()
+}
