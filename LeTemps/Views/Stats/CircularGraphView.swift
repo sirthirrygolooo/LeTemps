7da2b4d9e8d63fd54d8a6cde574d2a09bca82e87
hehe <hehe@gmail.com>
2025-03-25 08:32:49 +0100

    hehe

---
+++ LeTemps/Views/Stats/CircularGraphView.swift
@@ -0,0 +1,18 @@
+//
+//  CircularGraphView.swift
+//  LeTemps
+//
+//  Created by froehly jean-baptiste on 25/03/2025.
+//
+
+import SwiftUI
+
+struct CircularGraphView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircularGraphView()
+}
---
+++ LeTemps/Views/Stats/WeeklyStatisticsViews.swift
@@ -0,0 +1,18 @@
+//
+//  WeeklyStatisticsViews.swift
+//  LeTemps
+//
+//  Created by froehly jean-baptiste on 25/03/2025.
+//
+
+import SwiftUI
+
+struct WeeklyStatisticsViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeeklyStatisticsViews()
+}
