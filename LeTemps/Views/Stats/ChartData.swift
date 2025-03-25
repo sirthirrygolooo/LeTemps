4d5a75a8407e74e53d891bba2ca75cb28820de89
hehe <hehe@gmail.com>
2025-03-25 08:52:25 +0100

    hh

---
+++ LeTemps/Views/Stats/ChartData.swift
@@ -0,0 +1,8 @@
+//
+//  ChartData.swift
+//  LeTemps
+//
+//  Created by froehly jean-baptiste on 25/03/2025.
+//
+
+import Foundation
---
+++ LeTemps/Views/Stats/ChartDataContainer.swift
@@ -0,0 +1,8 @@
+//
+//  ChartDataContainer.swift
+//  LeTemps
+//
+//  Created by froehly jean-baptiste on 25/03/2025.
+//
+
+import Foundation
